3e192bf7d63f9955f103f6bc50c2ec9ccf8c4b9a
Paul Nechifor <irragal@gmail.com>
2014-01-26 04:30:02 +0200

    Added/hid page turning.

--- static/js/thumbs.js
+++ static/js/thumbs.js
@@ -4,6 +4,7 @@ function main() {
   });
 }
 
+// TODO: Refactor this ugliness.
 function wrapThumb(thumb) {
   var img = thumb.find('img');
   var pages = img.data('thumbs').split(' ');
@@ -27,7 +28,41 @@ function wrapThumb(thumb) {
     }
     bar.css('width', (((page + 1) / pages.length) * 100) + '%');
   });
+
+  //handleFlipClick(img, thumb.find('.flip-book'), pages);
 }
+
+/*
+jQuery.fn.center = function () {
+  this.css("position","absolute");
+  this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
+    $(window).scrollTop()) + "px");
+  this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
+    $(window).scrollLeft()) + "px");
+  return this;
+}
+
+function handleFlipClick(img, flipBook, pages) {
+
+  flipBook.click(function () {
+    var imgElem = img.get(0);
+
+    var div = $('<div/>').css({
+      width: (imgElem.naturalWidth * 2) + 'px',
+      height: imgElem.naturalHeight + 'px'
+    });
+
+    var wrapper = $('<div class="magazine"/>').appendTo($('body')).append(div).center();
+
+    for (var i = 0, len = pages.length; i < len; i++) {
+      var page = $('<div/>').appendTo(div);
+      $('<img/>').attr('src', storeUrl(pages[i])).appendTo(page);
+    }
+    var t = div.turn({gradients: true, acceleration: true});
+    window.t = t;
+  });
+}
+*/
 
 function storeUrl(hash) {
   return '/store/' + hash.substring(0, 3) + '/' + hash.substring(3);
