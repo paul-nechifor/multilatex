99455e666b7ba9c7bc0515a25a400f0af1290010
Paul Nechifor <paul@nechifor.net>
2014-06-02 00:40:51 +0300

    Convert `util.js` to CoffeeScript.

--- editor/util.js
+++
@@ -1,9 +0,0 @@
-exports.createElement = function (parent, type, classes) {
-  type = type || 'div';
-  var element = document.createElement(type);
-  parent.appendChild(element);
-  if (classes) {
-    element.setAttribute('class', classes);
-  }
-  return element;
-};
